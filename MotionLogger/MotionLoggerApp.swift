279e2cac2059459bf8732c82b326cbaabcb4e924
haruna1256 <haru_kawagishi@icloud.com>
2026-05-07 11:26:39 +0900

    Initial Commit

---
+++ MotionLogger/MotionLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionLoggerApp.swift
+//  MotionLogger
+//
+//  Created by 川岸遥奈 on 2026/05/07.
+//
+
+import SwiftUI
+
+@main
+struct MotionLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
